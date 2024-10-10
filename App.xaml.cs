6644cc2abb041422043d80032be8235732dc9257
140054 <140054@M42>
2024-10-10 15:12:00 +0200

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MaciejKloda_140054
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
