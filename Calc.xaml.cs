751a01ffb8e8805cfdfdf9e038917abcb53884b0
matthias2003 <56407111+matthias2003@users.noreply.github.com>
2024-10-17 16:21:00 +0200

    Lista1 (#1)
    * Finished List1
    
    * Made fixes to code from List1
    
    ---------
    
    Co-authored-by: 140054 <140054@M42>

---
+++ Calc.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MaciejKloda_140054
+{
+    public partial class Calc : Window
+    {
+        public Calc()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DockPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MaciejKloda_140054
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DockPanel.xaml
+    /// </summary>
+    public partial class DockPanel : Window
+    {
+        public DockPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
