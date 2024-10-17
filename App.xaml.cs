fce440204f290f7abd4f391fb311dc06a7437490
140054 <140054@M42>
2024-10-17 15:54:04 +0200

    Made fixes to code from List1

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace MaciejKloda_140054
 {
@@ -10,5 +8,4 @@ namespace MaciejKloda_140054
     public partial class App : Application
     {
     }
-
 }
--- Calc.xaml.cs
+++ Calc.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace MaciejKloda_140054
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class Calc : Window
     {
         public Calc()
