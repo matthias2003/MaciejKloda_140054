65b66f91bd03b10e75d2c27d8f1ebc96bd81a1be
140054 <140054@M42>
2024-10-17 17:07:39 +0200

    Started working on Calc

--- Calc.xaml.cs
+++ Calc.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace MaciejKloda_140054
 {
@@ -8,5 +9,40 @@ namespace MaciejKloda_140054
         {
             InitializeComponent();
         }
+
+        private void Button_Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            String btnContent = btn.Content.ToString();
+            if(btnContent == "." && !TextBox_Result.Text.Contains("."))
+            {
+                TextBox_Result.Text += btnContent;
+            } else if (btnContent != ".")
+            {
+                TextBox_Result.Text += btnContent;
+            }
+        }
+
+        private void Button_Sign_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            String btnContent = btn.Content.ToString();
+            String sign = "";
+            String value = "";
+
+            if (btnContent == "C")
+            {
+                TextBox_Result.Text = "";
+            } else if ( btnContent != "C" && btnContent != "=")
+            {
+                TextBox_Result.Text = "";
+                sign = btnContent;
+            } else if (btnContent == "=")
+            {
+                TextBox_Result.Text = value + " " + sign;
+            }
+
+        }
     }
+
 }
