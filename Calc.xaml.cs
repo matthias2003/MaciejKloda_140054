77d7bf630f01a89b21f2322f4902286d190f33a3
140054 <140054@M42>
2024-10-24 16:54:47 +0200

    Calc 0.1v

--- Calc.xaml.cs
+++ Calc.xaml.cs
@@ -1,10 +1,19 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace MaciejKloda_140054
 {
     public partial class Calc : Window
     {
+        private double tmpValue;
+        private string tmpSign;
+
+
+        private double firstValue;
+        private double secondValue;
+        private string sign;
+
         public Calc()
         {
             InitializeComponent();
@@ -27,21 +36,151 @@ namespace MaciejKloda_140054
         {
             Button btn = (Button)sender;
             String btnContent = btn.Content.ToString();
-            String sign = "";
-            String value = "";
 
             if (btnContent == "C")
             {
+                firstValue = 0;
+                secondValue = 0;
+                sign = "";
                 TextBox_Result.Text = "";
-            } else if ( btnContent != "C" && btnContent != "=")
-            {
-                TextBox_Result.Text = "";
-                sign = btnContent;
-            } else if (btnContent == "=")
-            {
-                TextBox_Result.Text = value + " " + sign;
             }
 
+            if (btnContent == "=")
+            {
+                double res = Operations(firstValue, secondValue, sign);
+                TextBox_Result.Text = "";
+            }
+
+            if (firstValue != 0)
+            {
+                secondValue = Double.Parse(TextBox_Result.Text);
+                double res = Operations(firstValue,secondValue,sign);
+                TextBox_Result.Text = "";
+                firstValue = res;
+            } else
+            {
+                firstValue = Double.Parse(TextBox_Result.Text);
+                sign = btnContent;
+                TextBox_Result.Text = "";
+            }
+
+
+            //switch (btnContent)
+            //{
+            //    case "C":
+            //        TextBox_Result.Text = "";
+            //        tmpSign = "";
+            //        tmpValue = 0;
+            //        break;
+            //    case "=":
+            //        if (tmpValue != 0)
+            //        {
+            //            double textBoxValue = Double.Parse(TextBox_Result.Text);
+            //            double res = Operations(tmpValue, textBoxValue, tmpSign);
+            //            TextBox_Result.Text = res.ToString();
+            //            tmpSign = "";
+            //            tmpValue = 0;
+            //        } else
+            //        {
+            //            tmpSign = "";
+            //            tmpValue = 0;
+            //        }
+            //        break;
+            //    case "+":
+            //        if (TextBox_Result.Text.Length > 0)
+            //        {
+            //            if (tmpValue != 0)
+            //            {
+            //                double textBoxValue = Double.Parse(TextBox_Result.Text);
+            //                tmpSign = "+";
+            //                double res = Operations(tmpValue, textBoxValue, tmpSign);
+            //                tmpValue = res;
+            //                TextBox_Result.Text = "";
+            //            } else
+            //            {
+            //                tmpValue = Double.Parse(TextBox_Result.Text);
+            //                tmpSign = "+";
+            //                TextBox_Result.Text = "";
+            //            }
+            //        }
+            //        break;
+            //    case "-":
+            //        if (TextBox_Result.Text.Length > 0)
+            //        {
+            //            if (tmpValue != 0)
+            //            {
+            //                double textBoxValue = Double.Parse(TextBox_Result.Text);
+            //                tmpSign = "-";
+            //                double res = Operations(tmpValue, textBoxValue, tmpSign);
+            //                tmpValue = res;
+            //                TextBox_Result.Text = "";
+            //            }
+            //            else
+            //            {
+            //                tmpValue = Double.Parse(TextBox_Result.Text);
+            //                tmpSign = "-";
+            //                TextBox_Result.Text = "";
+            //            }
+            //        }
+            //        break;
+            //    case "*":
+            //        if (TextBox_Result.Text.Length > 0)
+            //        {
+            //            if (tmpValue != 0)
+            //            {
+            //                double textBoxValue = Double.Parse(TextBox_Result.Text);
+            //                tmpSign = "*";
+            //                double res = Operations(tmpValue, textBoxValue,tmpSign);
+            //                tmpValue = res;
+            //                TextBox_Result.Text = "";
+            //            }
+            //            else
+            //            {
+            //                tmpValue = Double.Parse(TextBox_Result.Text);
+            //                tmpSign = "*";
+            //                TextBox_Result.Text = "";
+            //            }
+            //        }
+            //        break;
+            //    case "/":
+            //        if (TextBox_Result.Text.Length > 0)
+            //        {
+            //            if (tmpValue != 0)
+            //            {
+            //                double textBoxValue = Double.Parse(TextBox_Result.Text);
+            //                tmpSign = "/";
+            //                double res = Operations(tmpValue, textBoxValue, tmpSign);
+            //                tmpValue = res;
+            //                TextBox_Result.Text = "";
+            //            }
+            //            else
+            //            {
+            //                tmpValue = Double.Parse(TextBox_Result.Text);
+            //                tmpSign = "/";
+            //                TextBox_Result.Text = "";
+            //            }
+            //        }
+            //        break;
+                                    
+            //}
+        }
+        private double Operations(double firstValue, double secondValue, string sign)
+        {
+            switch (sign)
+            {
+                case "+":
+                    return firstValue + secondValue;
+                case "-":
+                    return firstValue - secondValue;
+                case "*":
+                    return firstValue * secondValue;
+                case "/":
+                    if (secondValue != 0) return firstValue / secondValue;
+                    MessageBox.Show("You mustn't divide by zero!");
+                    return 0;
+                default:
+                    return 0;
+            }   
         }
     }
 
