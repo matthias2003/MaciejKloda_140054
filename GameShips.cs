f21cd527016ac81cd77e3226a908b6267d4a8e6a
Maciej Kłoda <maciej.kloda98@gmail.com>
2024-11-01 19:56:39 +0100

    Finished Ships

---
+++ GameShips.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace MaciejKloda_140054
+{
+    internal class GameShips
+    {
+        private ObservableCollection<int> _PlayerOne = new ObservableCollection<int>{};
+        private ObservableCollection<int> _PlayerTwo = new ObservableCollection<int>{};
+
+        public ObservableCollection<int> PlayerOne
+        {
+            get
+            {
+                return _PlayerOne;
+            }
+            set
+            {
+               _PlayerOne = value;
+               OnPropertyChanged("PersonId");
+            }
+        }
+
+        public ObservableCollection<int> PlayerTwo
+        {
+            get
+            {
+                return _PlayerTwo;
+            }
+            set
+            {
+                _PlayerTwo = value;
+                OnPropertyChanged("PersonId");
+            }
+        }
+
+        public GameShips(int[] PersonOne, int[] PersonTwo)
+        {
+            foreach (int _person in PersonOne)
+            {
+                _PlayerOne.Add(_person);
+            }
+
+            foreach (int _person in PersonTwo)
+            {
+                _PlayerTwo.Add(_person);
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var eventHanlder = PropertyChanged;
+            if (eventHanlder != null) eventHanlder(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ShipsPlayerTwo.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MaciejKloda_140054
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ShipsPlayerTwo.xaml
+    /// </summary>
+    public partial class ShipsPlayerTwo : Window
+    {
+        public ShipsPlayerTwo()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click__Choose_Ships(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (((GameShips)this.DataContext).PlayerTwo[Convert.ToInt32(btn.Tag.ToString())] == 0)
+            {
+                ((GameShips)this.DataContext).PlayerTwo[Convert.ToInt32(btn.Tag.ToString())] += 1;
+            }
+            else if (((GameShips)this.DataContext).PlayerTwo[Convert.ToInt32(btn.Tag.ToString())] == 1)
+            {
+                ((GameShips)this.DataContext).PlayerTwo[Convert.ToInt32(btn.Tag.ToString())] -= 1;
+            }
+        }
+
+        private void Button_Click__Shoot(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (((GameShips)this.DataContext).PlayerOne[Convert.ToInt32(btn.Tag.ToString())] == 0 || ((GameShips)this.DataContext).PlayerOne[Convert.ToInt32(btn.Tag.ToString())] == 1)
+                ((GameShips)this.DataContext).PlayerOne[Convert.ToInt32(btn.Tag.ToString())] += 2;
+        }
+    }
+}
