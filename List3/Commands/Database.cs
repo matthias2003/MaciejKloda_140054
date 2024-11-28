e98aa1bcfa6773d690fef377b24e48aa10301275
Maciej Kłoda <maciej.kloda98@gmail.com>
2024-11-28 17:19:00 +0100

    Added database class

---
+++ List3/Commands/Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace List3.Commands
+{
+    public class Database
+    {
+        public void ConnectToDatabase()
+        {
+            string connectionString = "Server=tcp:server-maciejkloda.database.windows.net,1433;" +
+                                      "Initial Catalog=CarRental;" +
+                                      "Encrypt=True;" +
+                                      "TrustServerCertificate=False;" +
+                                      "Connection Timeout=30;" +
+                                      "Authentication=Active Directory Default;";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Połączono z bazą danych.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Błąd połączenia: {ex.Message}");
+            }
+        }
+    }
+}
